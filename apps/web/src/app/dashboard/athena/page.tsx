0b3aa367b8e7306429770d3256fecbb974e3107d
dysthemix <indicationsmedia@protonmail.com>
2026-07-28 11:51:28 -0600

    fix: unclosed div in athena page, add missing scroll-area component, set prod API URL default

--- apps/web/src/app/dashboard/athena/page.tsx
+++ apps/web/src/app/dashboard/athena/page.tsx
@@ -552,7 +552,6 @@ function AthenaPageContent() {
             )}
           </div>
         </div>
-      </div>
 
       {/* ── Main Chat Area ── */}
       <div className="flex-1 flex flex-col min-w-0">
@@ -853,5 +852,7 @@ function AthenaPageContent() {
         </div>
       )}
     </div>
+    </div>
+  </div>
   );
 }
